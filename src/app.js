ad97501bc2a66768df295beb947503391bab1ff7
Gbemmiey <gbemfemalabi@gmail.com>
2022-05-02 11:13:51 +0100

    Update: completed Components section

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
-const App = () => {
-    return React.createElement(
-        'div',
-        {},
-        React.createElement('h1', {id: "brand"}, 'Adopt Me')
-    );
-}
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+    React.createElement("hr", {})
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "brand" }, "Adopt Me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "hawaii",
+    }),
+    React.createElement(Pet, {
+      name: "Suse",
+      animal: "cat",
+      breed: "local",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "parrot",
+      breed: "unknown",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
